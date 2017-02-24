f76483d3cfa3cb42d7bb7165e76ba59a97ac475d
Zhaeong <owen_zzz@hotmail.com>
2017-02-24 02:15:38 -0500

    changed login to api

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 Visit the below endpoints<br/>
 
 Login:<br/>
-http://192.168.2.34/android_booya_login/login.php<br/><br/>
+http://192.168.2.34/android_booya_api/login.php<br/><br/>
 
 Registration:<br/>
-http://192.168.2.34/android_booya_login/register.php<br/><br/>
+http://192.168.2.34/android_booya_api/register.php<br/><br/>
