a3358f1d5cc34363cbf91c2337c1fb55129b5ddc
Zhaeong <owen_zzz@hotmail.com>
2017-02-24 15:08:08 -0500

    removed tut authorship

--- login.php
+++ login.php
@@ -1,10 +1,4 @@
 <?php
-
-/**
- * @author Ravi Tamada
- * @link http://www.androidhive.info/2012/01/android-login-and-registration-with-php-mysql-and-sqlite/ Complete tutorial
- */
-
 require_once 'include/DB_Functions.php';
 $db = new DB_Functions();
 
